b5f41bc57a5755ecd2d29981ef9aec06d47d7561
Bartek Stepien <nerdianin@gmail.com>
2016-01-25 11:37:20 +0100

    Created class konstverk

---
+++ Konstverk.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OGTavlor
+{
+    class Konstverk
+    {
+        public int Id { get; set; }
+        public string Namn { get; set; }
+        public string Datum { get; set; }
+        public string Konstnär { get; set; }
+        public string Rum { get; set; }
+        public string Material { get; set; }
+        public int Bredd { get; set; }
+        public int Höjd { get; set; }
+        public string Kommentar { get; set; }
+
+    }
+}
