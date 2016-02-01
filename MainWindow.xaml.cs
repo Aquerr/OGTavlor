1c999b235d1d46dcda0a12fdcb745a66c25562a9
Aquerr <nerdianin@gmail.com>
2016-02-01 15:05:02 +0100

    Added "OnClick" event to the first picture.
    Changed ListView's name to ArtworkListView.
    Added new method "FillList" which will be used to fill data in ListView.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,14 +24,7 @@ namespace TavelProjektPT
         public MainWindow()
         {
             InitializeComponent();
-            List<User> users = new List<User>();
-            users.Add(new User() { Id = 1, Name = "John Doe", Date = new DateTime(1971, 7, 23).ToString(), ImageUrl= "http://www.wpf-tutorial.com/images/misc/john_doe.jpg" }); //Temporary
-            users.Add(new User() { Id = 2, Name = "Jane Doe", Date = new DateTime(1974, 1, 17).ToString() }); //Temp
-            users.Add(new User() { Id = 3, Name = "Sammy Doe", Date = new DateTime (1991, 9, 2).ToString() }); //Temp
-             //lvDataBinding = namn på ListView
-
-            
-         //    lvDataBinding.ItemsSource = users;
+            FillList();
         }      
 
         private void button_Click(object sender, RoutedEventArgs e)
@@ -79,7 +72,6 @@ namespace TavelProjektPT
         {
             SearchBox.Text = "";
         }
-    }
     
 
         public class User //Temp! ersätts sedan med klassen Konstverk MEN INTE ÄN!!!
@@ -95,4 +87,31 @@ namespace TavelProjektPT
             //    return this.Name + ", " + this.Age + " years old";
             //}
         }
+
+        public void FillList()
+        {
+            List<User> users = new List<User>();
+            users.Add(new User() { Id = 1, Name = "John Doe", Date = new DateTime(1971, 7, 23).ToString(), ImageUrl = "http://www.wpf-tutorial.com/images/misc/john_doe.jpg" }); //Temporary
+            users.Add(new User() { Id = 2, Name = "Jane Doe", Date = new DateTime(1974, 1, 17).ToString() }); //Temp
+            users.Add(new User() { Id = 3, Name = "Sammy Doe", Date = new DateTime(1991, 9, 2).ToString() }); //Temp
+                                                                                                              //lvDataBinding = namn på ListView
+
+
+            //    lvDataBinding.ItemsSource = users;
+
+            foreach (var item in users)
+            {
+             //   ArtworkListView.ItemsPanel.Template
+            }
+        }
+
+        private void PictureClick(object sender, RoutedEventArgs e)
+        {
+            Window picture1 = new Window();
+            picture1.Title = "Bildspel";
+            picture1.Height = 720;
+            picture1.Width = 1280;
+            picture1.Show();
+        }
+    }
 }
